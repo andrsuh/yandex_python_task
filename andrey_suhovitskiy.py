d6056c11a15be82d8ddede4ee4ea601e824338ef
andrsuh <andrsuh@rambler.ru>
2016-05-07 22:46:57 +0300

    pep8 fixes

--- andrey_suhovitskiy.py
+++ andrey_suhovitskiy.py
@@ -1,15 +1,13 @@
 import sys
-import os
 
 
-def func(filename):
-    BUFFER_SIZE = 1024 * 1024
-
+def process(filename):
     FROM = "From ".encode()
-    # BUFFER_SIZE = 65536 * 2
-    users_file = {None: open(os.devnull, 'wb', buffering=BUFFER_SIZE)}
-
+    BUFFER_SIZE = 65536 * 16
+    # BUFFER_SIZE = 65536
+    users_file = {}
     current_user = None
+    fld = "users/"
 
     for b_line in open(filename, "rb", buffering=BUFFER_SIZE):
         if b_line.startswith(FROM):
@@ -17,36 +15,19 @@ def func(filename):
             current_user = users_email
 
             if current_user not in users_file:
-                users_file[current_user] = open(
-                                            "users/" + current_user.decode(),
-                                            "wb",
-                                             buffering=BUFFER_SIZE
-                                            )
+                users_file[current_user] = open(fld + current_user.decode(),
+                                                "wb",
+                                                buffering=BUFFER_SIZE)
 
-        # if current_user:
-        users_file[current_user].write(b_line)
+        if current_user:
+            users_file[current_user].write(b_line)
 
     for file in users_file.values():
         file.close()
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    if len(sys.argv) != 2:
+        print("filename is not define")
         exit(1)
-    func(sys.argv[1])
-
-    # def func_1():
-    #     for line in open("maxtest.mailbox", errors="ignore", buffering=BUFFER_SIZE):
-    #         if line.startswith("From "):
-    #             users_email = line.split(" ")[1]
-    #             current_user = users_email
-    #             # print(current_user)
-    #
-    #             if current_user not in users_file:
-    #                 users_file[current_user] = open("users/" + current_user, "w", buffering=BUFFER_SIZE)
-    #
-    #         if current_user:
-    #             users_file[current_user].write(line)
-    #
-    #     for file in users_file.values():
-    #         file.close()
+    process(sys.argv[1])
